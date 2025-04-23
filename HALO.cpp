deaefaab3b338e223dd9285b77d8a699331f5bc7
Khaerul Umam <108923145+akangumam@users.noreply.github.com>
2025-04-23 19:09:25 +0700

    Add files via upload

---
+++ HALO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string kata;
+  cout << "Masukkan kata = HALO" << endl;
+  cin >> kata;
+
+  if (kata == "HALO") {
+    cout << "Kata yang dimasukkan sesuai" << endl;
+  } else {
+    cout << "Kata yang dimasukkan tidak sesuai" << endl;
+  }
+
+  return 0;
+}
