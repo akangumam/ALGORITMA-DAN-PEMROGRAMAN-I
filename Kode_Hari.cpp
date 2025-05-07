6c20210b7a8d468b0ccb703e15428cc9a77832f0
Khaerul Umam <108923145+akangumam@users.noreply.github.com>
2025-05-07 13:17:25 +0700

    Add files via upload

---
+++ Kode_Hari.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Identitas
+    char nama[] = "Khaerul Umam";
+    char nim[] = "241011450666";
+
+    // Tampilkan identitas
+    cout << "==============================" << endl;
+    cout << "Nama : " << nama << endl;
+    cout << "NIM  : " << nim << endl;
+    cout << "Program :Switch Kode Hari" << endl;
+    cout << "==============================" << endl << endl;
+
+    // Variabel
+    int hari;
+    cout << "Masukan Kode Hari : ";
+    cin >> hari;
+
+    switch (hari) {
+        case 1:
+            cout << "Senin";
+            break;
+        case 2:
+            cout << "Selasa";
+            break;
+        case 3:
+            cout << "Rabu";
+            break;
+        case 4:
+            cout << "Kamis";
+            break;
+        case 5:
+            cout << "Jumat";
+            break;
+        case 6:
+            cout << "Sabtu";
+            break;
+        case 7:
+            cout << "Minggu";
+            break;
+        default:
+            cout << "Tidak Ada Hari";
+    }
+
+    return 0;
+}
+
