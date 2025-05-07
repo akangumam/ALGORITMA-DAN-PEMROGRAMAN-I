b90339d3865f19b74e8c4808052a252d84336ba6
Khaerul Umam <108923145+akangumam@users.noreply.github.com>
2025-05-07 11:57:30 +0700

    Update and rename fungsi strcmp.cpp to 8_FungsiStrcmp.cpp

--- 8_FungsiStrcmp.cpp
+++ 8_FungsiStrcmp.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+	// Identitas
+    char nama[] = "Khaerul Umam";
+    char nim[] = "241011450666";
+
+    // Tampilkan identitas
+    cout << "==============================" << endl;
+    cout << "Nama : " << nama << endl;
+    cout << "NIM  : " << nim << endl;
+    cout << "Program : Fungsi strcmp" << endl;
+    cout << "==============================" << endl << endl;
+
+    // Variabel
     char A[15];
     char B[15];
 
