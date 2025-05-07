1a6d160e0fb582526009f409da6d76496447c0b0
Khaerul Umam <108923145+akangumam@users.noreply.github.com>
2025-05-07 13:24:51 +0700

    Add files via upload

---
+++ Luas_Lingkaran.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Langkah 1: Definisikan jari-jari dan phi
+    float phi = 3.14, r = 7, luas;
+
+    // Langkah 2: Hitung luas
+    luas = phi * r * r;
+
+    // Langkah 3: Cetak hasil
+    cout << "Luas lingkaran dengan jari-jari 7 cm adalah: " << luas << " cm²" << endl;
+
+    return 0;
+}
